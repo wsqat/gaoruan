cbf3fee64314630283f821991f584a58b19a1d3c
wsqat <786607676@qq.com>
2017-09-15 12:45:57 +0800

    modify README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gaoruan
-高软课程实验代码
+##高软课程实验代码
+
+###第一周作业
+   |__ lab1/   hello word的程序
+   |__ todos/  使用vuejs完成的ToDoList
---
+++ todos/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <h1 v-text="title"></h1>
+    <input v-model="newItem" v-on:keyup.enter="addNew">
+    <ul>
+      <li v-for="item in items" v-bind:class="{finished:item.isFinished}"
+       v-on:click="toggleFinish(item)">{{item.label}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Store from './store'
+export default {
+  data () {
+    return {
+      title: 'this is a todo list',
+      items: Store.fetch(),
+      newItem: ''
+    }
+  },
+  watch: {
+    items: {
+      handler: function(items){
+        Store.save(items)
+      },
+      deep: true
+    }
+  },
+  methods: {
+    toggleFinish:function (item) {
+      // console.log(item)
+      item.isFinished = !item.isFinished
+    },
+    addNew:function(){
+      // console.log(this.newItem)
+      this.items.push({
+        label: this.newItem,
+        isFinished: false
+      })
+      this.newItem = ''
+    }
+  }
+}
+</script>
+
+<style>
+.finished{
+  text-decoration: underline;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
